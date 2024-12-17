e42fb789fbaba4c99756cd7ecc062f26f7030ad2
davy1ex <davy1ex@yandex.ru>
2024-12-17 12:52:57 +0300

    f

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    padavyblog "🕶️ davyblog",
+    pageTitle: "🕶️ davyblog",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
